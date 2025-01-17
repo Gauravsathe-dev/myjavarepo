17586b61d63d12984069001fdc1ade80108635e5
Gaurav <sathegaurav2002@gmail.com>
2025-01-17 18:05:44 +0000

    second commit

--- pj2.java
+++ pj2.java
@@ -1,5 +1,5 @@
 class pj2 {
     public static void main(String[] args) {
-        System.out.println("Hello, World!"); 
+        System.out.println("Hello this is my first java program...!"); 
     }
 }  
