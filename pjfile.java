a68baa17b4e1aa12016b115a8fcc873b6d1c4729
Gaurav <sathegaurav2002@gmail.com>
2025-01-18 17:56:05 +0000

    committed

--- pjfile.java
+++ pjfile.java
@@ -2,7 +2,7 @@ class pjfile
 {
     public static void main(String args[])
     {
-        System.out.print("Wlcome to my project file...!");
+        System.out.print("Wlcome to my project file for once again.Thank you...!");
     }
 }
 
